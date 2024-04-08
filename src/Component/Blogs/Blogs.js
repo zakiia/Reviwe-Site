4202250e1017dc4ca8adcfe1ec01f61a7ed46078
zakiia <zakiasultanabhuiyan@gmail.com>
2024-04-08 21:14:48 +0600

    making blog

--- src/Component/Blogs/Blogs.js
+++ src/Component/Blogs/Blogs.js
@@ -1,9 +1,21 @@
 import React from "react";
+import "./Blogs.css";
 
 const Blogs = () => {
   return (
-    <div>
-      <h2>This is blogs</h2>
+    <div className="blog">
+      <h2>1. What is the purpose of context API</h2>
+      <p>
+        Context API is used to pass global variables anywhere in the code. It
+        helps when there is a need for sharing state between a lot of nested
+        components. It is light in weight and easier to use, to create a context
+        just need to call React
+      </p>
+      <h2>2. What is semantic tag?</h2>
+      <p>
+        A semantic element clearly describes its meaning to both the browser and
+        the developer. Examples of non-semantic elements:- div and span.
+      </p>
     </div>
   );
 };
